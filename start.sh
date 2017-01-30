727966a8152cbbe8368a870e113a62d69fc3f4cb
Alan Dipert <alan@dipert.org>
2017-01-30 10:25:54 -0800

    Optional SSL redirection
    - Added two variables to configure optional SSL redirection
    - One variable turns redirection on, the other configures which host to
    redirect TO
    - Use the x-forwarded-proto header because ELB serves the SSL and we
    want to avoid infinite loop.

--- start.sh
+++ start.sh
@@ -3,6 +3,8 @@
 VARNISH_BACKEND_PORT=${VARNISH_BACKEND_PORT-80}
 VARNISH_BACKEND_ELB=${VARNISH_BACKEND_ELB-example.com}
 VARNISH_BACKEND_HOST=${VARNISH_BACKEND_HOST-example.com}
+VARNISH_BACKEND_FORCE_SSL=${VARNISH_BACKEND_FORCE_SSL-false}
+VARNISH_BACKEND_FORCE_SSL_HOST=${VARNISH_BACKEND_FORCE_SSL-example.com}
 
 function replace_env() {
   local file="${1?}"
@@ -14,7 +16,12 @@ function replace_env() {
   done
 }
 
-replace_env /etc/nginx/nginx.conf VARNISH_BACKEND_PORT VARNISH_BACKEND_ELB VARNISH_BACKEND_HOST
+replace_env /etc/nginx/nginx.conf \
+            VARNISH_BACKEND_PORT \
+            VARNISH_BACKEND_ELB \
+            VARNISH_BACKEND_HOST \
+            VARNISH_BACKEND_FORCE_SSL \
+            VARNISH_BACKEND_FORCE_SSL_HOST
 
 if [ -n "$DATADOG_API_KEY" ]; then
   replace_env /etc/dd-agent/datadog.conf DATADOG_API_KEY DATADOG_TAGS
